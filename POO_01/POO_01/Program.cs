b2e988227dbe8c7db2e249e5dd0b46813fb29de4
LeonardoPena <61291132+LeonardoPena@users.noreply.github.com>
2020-02-27 11:29:07 -0400

    Add files via upload

---
+++ POO_01/POO_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace POO_01
+{
+    class mostrarVehiculo
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Vehiculo Carro1 = new Vehiculo();
+            Vehiculo Carro2 = new Vehiculo();
+
+            string marca, modelo;
+            Console.Write("Vehiculo1: \n\n\nIngrese la Marca del vehiculo:");
+            marca = Console.ReadLine();
+            Console.Write("Ingrese el Modelo del vehiculo:");
+            modelo = Console.ReadLine();
+
+            Carro1.SetVehiculo(marca, modelo);
+            Console.Clear();
+
+            Console.Write("Vehiculo 2: \n\n\nIngrese la Marca del vehiculo:");
+            marca = Console.ReadLine();
+            Console.Write("Ingrese el Modelo del vehiculo:");
+            modelo = Console.ReadLine();
+
+            Carro2.SetVehiculo(marca, modelo);
+
+            Console.ReadKey();
+
+            Console.Clear();
+            Carro1.verVehiculo();
+            Carro2.verVehiculo();
+
+            
+        }
+    }
+}
---
+++ POO_01/POO_01/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_01
+{
+    class Vehiculo
+    {
+        private string marca;
+        private string modelo;
+        
+        public void SetVehiculo(string marc, string model)
+        {
+            marca = marc;
+            modelo = model;
+        }
+        public void verVehiculo()
+        {
+            Console.WriteLine("El Vehiculo es:  "+marca+"\n Modelo: "+modelo);
+            Console.ReadKey();
+        }
+    }
+}
